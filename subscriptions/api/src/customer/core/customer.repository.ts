f116df44dcc35eb2bb3f039dbc9bdfd127c6f75c
Jeremy PELE <jeremy.pele@gmail.com>
2020-11-02 16:14:05 +0000

    Remove boilerplate app*

--- subscriptions/api/src/customer/core/customer.repository.ts
+++ subscriptions/api/src/customer/core/customer.repository.ts
@@ -1,10 +1,5 @@
 import { Customer } from './customer.root.entity';
 
-export interface CustomerQueryParams {
-  firstName?: string;
-  lastName?: string;
-}
-
 export interface CustomerRepository {
   findAll(): Promise<Customer[]>;
 }
