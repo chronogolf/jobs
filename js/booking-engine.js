9390ba56eb12c6e122205ea47ad860fefbb48735
Tejbir Wason <tejbirwason@gmail.com>
2021-10-22 10:39:14 -0500

    Update booking-engine.js

--- js/booking-engine.js
+++ js/booking-engine.js
@@ -1,9 +1,8 @@
 /*
-Setup:
-1. Pull the public GH repo jobs
-2. Run using `node js/booking-engine.js`
+Running instructions:
+1. Run using `node js/booking-engine.js`
 
-Instructions:
+Challend:
 1. We wish to create a reservation booking engine service that can book reservation for a golfer by name and time of day.
 2. We want to extend the capability of the reservation engine to only allow reservations to be booked at 10 minute increments
 3. Further, we wish to add the ability to updated existing reservations
